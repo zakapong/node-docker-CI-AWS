d5850befead2c2bb707f243c2272fb42a764952a
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-19 10:19:19 +0100

    breaking app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// Sample node.js web app for Pluralsight Docker CI course
-// For demonstration purposes only
+// Sample node.js web app for Docker CI
+// For demonstration
+// this is it
 'use strict';
 
-var express = require('express'),
+var express = require('/express'),
     app = express();
 
-app.set('views', '/views');
+app.set('views', 'views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
